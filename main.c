438ba9ac89da21019f075cfd2b4e371f4066fbbc
Jorge Lucas <lukasrochaaraujo@hotmail.com>
2022-02-08 20:32:12 -0300

    Remoção de consultas fora dos requisitos.

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAXIMO_DIAS_SEMANA 5
+#define MAXIMO_CONSULTA_DIA_POR_MEDICO 2
 #define MAXIMO_MEDICOS 3
 #define MAXIMO_PACIENTES 5
-#define OPCAO_SAIR 0
+
 #define OPCAO_MEDICO_CADASTRAR 1
-#define OPCAO_MEDICO_CONSULTAR 2
-#define OPCAO_PACIENTE_CADASTRAR 3
-#define OPCAO_PACIENTE_CONSULTAR 4
+#define OPCAO_PACIENTE_CADASTRAR 2
+#define OPCAO_CONSULTA_CADASTRAR 3
+#define OPCAO_PESQUISAR_CONSULTA 4
+#define OPCAO_SAIR 0
 
 //estruturas
 struct Medico
@@ -31,46 +34,61 @@ struct Consulta
     int Codigo;
     int CodigoMedico;
     int CodigoPaciente;
-    int DiaSemana;
-    int Hora;
+    char DiaSemana[3];
+    char Hora[5];
 };
 
 //declaracoes de funcoes
+void ConfigurarMatrizDiasSenama();
 void Menu();
 
 void CadastrarMedico();
-void ConsultarMedico();
 void ExibirMedico(struct Medico medico);
 
 void CadastrarPaciente();
-void ConsultarPaciente();
 void ExibirPaciente(struct Paciente paciente);
+
+void CadastrarConsulta();
+void PesquisarConsulta();
+void ExibirConsulta(struct Consulta consulta);
 
 void LimparTela();
 
 //variaveis globais
-struct Medico matriz_medico[MAXIMO_MEDICOS];
-struct Paciente matriz_paciente[MAXIMO_PACIENTES];
+struct Medico matriz_medico[MAXIMO_MEDICOS + 1];
+struct Paciente matriz_paciente[MAXIMO_PACIENTES + 1];
+struct Consulta matriz_consulta[MAXIMO_DIAS_SEMANA][MAXIMO_MEDICOS * MAXIMO_CONSULTA_DIA_POR_MEDICO];
+char* matriz_dia_semana[5];
 
 int main()
 {
-    printf("\n[BEM VINDO]\n\n");
-
+    ConfigurarMatrizDiasSenama();
     Menu();
 
     return 0;
 }
 
+void ConfigurarMatrizDiasSenama()
+{
+    matriz_dia_semana[0] = "SEG";
+    matriz_dia_semana[1] = "TER";
+    matriz_dia_semana[2] = "QUA";
+    matriz_dia_semana[3] = "QUI";
+    matriz_dia_semana[4] = "SEX";
+}
+
 void Menu()
 {
+    printf("\n[BEM VINDO]\n\n");
+
     for(;;)
     {
         int opcao_menu;
         printf("[MENU] - ESCOLHA UMA OPCAO ABAIXO:\n\n");
         printf("\t[1]. CADASTRAR MEDICO\n\n");
-        printf("\t[2]. CONSULTAR MEDICO\n\n");
-        printf("\t[3]. CADASTRAR PACIENTE\n\n");
-        printf("\t[4]. CONSULTAR PACIENTE\n\n");
+        printf("\t[2]. CADASTRAR PACIENTE\n\n");
+        printf("\t[3]. CADASTRAR CONSULTA\n\n");
+        printf("\t[4]. PESQUISAR CONSULTA\n\n");
         printf("\t[0]. SAIR\n\n");
         printf("OPCAO: ");
         scanf("%d", &opcao_menu);
@@ -83,14 +101,14 @@ void Menu()
             case OPCAO_MEDICO_CADASTRAR:
                 CadastrarMedico();
                 break;
-            case OPCAO_MEDICO_CONSULTAR:
-                ConsultarMedico();
-                break;
             case OPCAO_PACIENTE_CADASTRAR:
                 CadastrarPaciente();
                 break;
-            case OPCAO_PACIENTE_CONSULTAR:
-                ConsultarPaciente();
+            case OPCAO_CONSULTA_CADASTRAR:
+                CadastrarConsulta();
+                break;
+            case OPCAO_PESQUISAR_CONSULTA:
+                PesquisarConsulta();
                 break;
             case OPCAO_SAIR:
                 printf("\n[ATE LOGO]\n\n");
@@ -133,74 +151,12 @@ void CadastrarMedico()
     if (medicoExistente.Codigo > 0)
     {
         printf("\n[O CODIGO INFORMADO JA ESTA EM USO]\n");
-
-        char respostaSobrescrever = 'n';
-        printf("\n[DESEJA SOBRESCREVER? (S|N)]: ");
-        scanf("%s", &respostaSobrescrever);
-        getchar();
-
-        if (respostaSobrescrever == 'S' || respostaSobrescrever == 's')
-        {
-            matriz_medico[novoMedico.Codigo] = novoMedico;
-            printf("\n[MEDICO ATUALIZADO]\n\n");
-        }
     }
     else
     {
         matriz_medico[novoMedico.Codigo] = novoMedico;
         printf("\n[MEDICO CADASTRADO]\n\n");
     }
-}
-
-void ConsultarMedico()
-{
-    int codigoMedico;
-
-    printf("\n[CONSULTAR MEDICO]\n\n");
-    printf("CODIGO....: ");
-    scanf("%d", &codigoMedico);
-    getchar();
-
-    struct Medico medico = matriz_medico[codigoMedico];
-
-    if(medico.Codigo > 0)
-    {
-        printf("\n[MEDICO ENCONTRADO]\n\n");
-        ExibirMedico(medico);
-    }
-    else
-    {
-        printf("\n[MEDICO NAO ENCONTRADO]\n");
-
-        char respostaListarTudo = 'n';
-        printf("\n[DESEJA EXIBIR TODOS OS MEDICOS? (S|N)]: ");
-        scanf("%s", &respostaListarTudo);
-        getchar();
-
-        if (respostaListarTudo == 'S' || respostaListarTudo == 's')
-        {
-            printf("\n[MEDICOS CADASTRADOS]\n\n");
-            for(int codigo = 0; codigo < MAXIMO_MEDICOS; codigo++)
-            {
-                struct Medico medicoBusca;
-                medicoBusca = matriz_medico[codigo];
-
-                if (medicoBusca.Codigo > 0)
-                {
-                    ExibirMedico(medicoBusca);
-                    printf("------------------------------------------\n");
-                }
-            }
-        }
-    }
-}
-
-void ExibirMedico(struct Medico medico)
-{
-    printf("CODIGO....: %d \n", medico.Codigo);
-    printf("NOME......: %s \n", medico.Nome);
-    printf("ENDERECO..: %s \n", medico.Endereco);
-    printf("TELEFONE..: %s \n", medico.Telefone);
 }
 
 void CadastrarPaciente()
@@ -232,17 +188,6 @@ void CadastrarPaciente()
     if (pacienteExistente.Codigo > 0)
     {
         printf("\n[O CODIGO INFORMADO JA ESTA EM USO]\n");
-
-        char respostaSobrescrever = 'n';
-        printf("\n[DESEJA SOBRESCREVER? (S|N)]: ");
-        scanf("%s", &respostaSobrescrever);
-        getchar();
-
-        if (respostaSobrescrever == 'S' || respostaSobrescrever == 's')
-        {
-            matriz_paciente[novoPaciente.Codigo] = novoPaciente;
-            printf("\n[PACIENTE ATUALIZADO]\n\n");
-        }
     }
     else
     {
@@ -251,55 +196,48 @@ void CadastrarPaciente()
     }
 }
 
-void ConsultarPaciente()
+void CadastrarConsulta()
 {
-    int codigoPaciente;
+    printf("\n[CADASTRAR CONSULTA]\n\n");
 
-    printf("\n[CONSULTAR PACIENTE]\n\n");
-    printf("CODIGO....: ");
-    scanf("%d", &codigoPaciente);
+    //selecao dia semana
+    printf("\n[SELECIONE UM DIA DA SEMANA]\n\n");
+
+    for(int diaSemana = 0; diaSemana < 5; diaSemana++)
+        printf("[%d]-%s  ", diaSemana, matriz_dia_semana[diaSemana]);
+
+    printf("\n");
+
+    int diaSemana;
+    printf("\nOPCAO: ");
+    scanf("%d", &diaSemana);
     getchar();
 
-    struct Paciente paciente = matriz_paciente[codigoPaciente];
+    printf("\n[DIA SELECIONADO] - [%s]\n", matriz_dia_semana[diaSemana]);
 
-    if(paciente.Codigo > 0)
-    {
-        printf("\n[PACIENTE ENCONTRADO]\n\n");
-        ExibirPaciente(paciente);
-    }
-    else
-    {
-        printf("\n[PACIENTE NAO ENCONTRADO]\n");
+    //selecao medico (lembrar limite consultas/dia)
+    printf("\n[SELECIONE UM MEDICO]\n");
+    getchar();
 
-        char respostaListarTudo = 'n';
-        printf("\n[DESEJA EXIBIR TODOS OS PACIENTE? (S|N)]: ");
-        scanf("%s", &respostaListarTudo);
-        getchar();
+    //selcao paciente
+    printf("\n[SELECIONE UM PACIENTE]\n");
+    getchar();
 
-        if (respostaListarTudo == 'S' || respostaListarTudo == 's')
-        {
-            printf("\n[PACIENTES CADASTRADOS]\n\n");
-            for(int codigo = 0; codigo < MAXIMO_PACIENTES; codigo++)
-            {
-                struct Paciente pacienteBusca;
-                pacienteBusca = matriz_paciente[codigo];
-
-                if (pacienteBusca.Codigo > 0)
-                {
-                    ExibirPaciente(pacienteBusca);
-                    printf("------------------------------------------\n");
-                }
-            }
-        }
-    }
+    //registrar consulta
 }
 
-void ExibirPaciente(struct Paciente paciente)
+void PesquisarConsulta()
 {
-    printf("CODIGO....: %d \n", paciente.Codigo);
-    printf("NOME......: %s \n", paciente.Nome);
-    printf("ENDERECO..: %s \n", paciente.Endereco);
-    printf("TELEFONE..: %s \n", paciente.Telefone);
+
+}
+
+void ExibirConsulta(struct Consulta consulta)
+{
+    printf("CODIGO.....: %d \n", consulta.Codigo);
+    printf("DIA SEMANA.: %s \n", consulta.DiaSemana);
+    printf("HORARIO....: %s \n", consulta.Hora);
+    printf("MEDICO.....: %d \n", consulta.CodigoMedico);
+    printf("PACIENTE...: %d \n", consulta.CodigoPaciente);
 }
 
 void LimparTela()
